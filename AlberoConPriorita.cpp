a5568e63be15aca447ca206c16c39fe82bed831b
Gioele <gcageggi@gmail.com>
2018-07-04 11:59:17 +0200

    upgraded

--- AlberoConPriorita.cpp
+++ AlberoConPriorita.cpp
@@ -1,149 +1,154 @@
-#include <iostream>
+#include<iostream>
 
- using namespace std;
+using namespace std;
 
- template<class H> class Nodo{
- private:
-   H *key;
-   int p;
-   Nodo<H> *left, *right, *parent;
-   Nodo();
- public:
-   Nodo(int p,H key){
-     this->key=new H(key);
-     this->p=p;
-     parent=right=left=NULL;
-   }
+template<class H> class PBT{
+public:
+  virtual PBT<H> *ins(int priority, H key)=0;
+  virtual H find(int priority)=0;
+  virtual void print()=0;
+};
 
-   void setKey(H key){this->key=new H(key);}
-   void setLeft(Nodo<H> *x){left=x;}
-   void setRight(Nodo<H> *x){right=x;}
-   void setParent(Nodo<H> *x){parent=x;}
-   void setPriority(int x){p=x;}
 
-   H *getKey(){return key;}
-   Nodo<H> *getLeft(){return left;}
-   Nodo<H> *getRight(){return right;}
-   Nodo<H> *getParent(){return parent;}
-   int getPriority(){return p;}
- };
+template<class H> class Nodo{
+private:
+  H *key;
+  Nodo<H> *left, *right, *parent;
+  int priority;
+public:
+  Nodo(int p, H key){
+    this->key=new H(key);
+    priority=p;
+    left=right=parent=NULL;
+  }
 
- template<class H> class PBT{
- private:
-   int n;
-   Nodo<H> *root, *current;
+  void setKey(H key){this->key=new H(key);}
+  void setPriority(int p){priority=p;}
+  void setLeft(Nodo<H> *x){left=x;}
+  void setRight(Nodo<H> *x){right=x;}
+  void setParent(Nodo<H> *x){parent=x;}
 
-   void _inoderReverse(Nodo<H> *x){
-     if(x){
-       _inoderReverse(x->getRight());
-       cout << *x->getKey() << ", ";
-       _inoderReverse(x->getLeft());
-     }
-   }
+  H *getKey(){return key;}
+  int getPriority(){return priority;}
+  Nodo<H> *getLeft(){return left;}
+  Nodo<H> *getRight(){return right;}
+  Nodo<H> *getParent(){return parent;}
 
-   Nodo<H> *_rfind(int p, Nodo<H> *tmp){
-     if(!tmp) return NULL;
-     if(p == tmp->getPriority()) return tmp;
-     if(p > tmp->getPriority()) return _rfind(p, tmp->getRight());
-     return _rfind(p, tmp->getLeft());
-   }
+};
 
-   Nodo<H> *_rsearch(H key, Nodo<H> *tmp){
-     if(!tmp) return NULL;
-     if(key == *tmp->getKey()) return tmp;
-     if(key > *tmp->getKey()) return _rsearch(key, tmp->getRight());
-     return _rsearch(key, tmp->getLeft());
-   }
+template<class H> class MyPBT{
+private:
+  int n;
+  Nodo<H> *root, *current;
 
-   Nodo<H> *_minimo(Nodo<H> *tmp){
-     while(tmp->getLeft()) tmp=tmp->getLeft();
-     return tmp;
-   }
-
-   Nodo<H>* _successore(Nodo<H>* tmp) {
-     if(tmp->getRight())
-       return _minimo(tmp->getRight());
-     Nodo<H> *p = tmp->getParent();
-     while(p!=NULL && *p->getKey()<*tmp->getKey())
-       p = p->getParent();
-     return p;
-   }
-
-   void _canc(Nodo<H> *nd, H key){
-     Nodo<H> *tmp = _rsearch(key, nd);
-     if(!tmp) return;
-     if(!tmp->getLeft() || !tmp->getRight()){
-       n--;
-       Nodo<H> *parent = tmp->getParent(),
-               *figlio = tmp->getLeft();
-       if(tmp->getRight()) figlio= tmp->getRight();
-      if(!parent) root = figlio;
-      else{
-        if(tmp == parent->getLeft()) parent->setLeft(figlio);
-        else parent->setRight(figlio);
-      }
-      if(figlio) figlio->setParent(parent);
-    }else{
-      Nodo<H> *succ = _successore(tmp);
-      tmp->setKey(*succ->getKey());
-      tmp->setPriority(succ->getPriority());
-      _canc(tmp->getRight(), *succ->getKey());
-
+  void _revInOrder(Nodo<H> *x){
+    if(x){
+      _revInOrder(x->getRight());
+      cout << *x->getKey() << " ";
+      _revInOrder(x->getLeft());
     }
-    return;
-   }
+  }
 
- public:
-   PBT(){
-     root=NULL;
-     n=0;
-   }
+  Nodo<H> *_rfind(Nodo<H> *x, int priority){
+    if(!x) return NULL;
+    if(x->getPriority() == priority) return x;
+    if(priority>x->getPriority()) return _rfind(x->getRight(), priority);
+    return _rfind(x->getLeft(), priority);
+  }
 
-   PBT<H> *insert(int p, H key){
-     Nodo<H> *tmp=root,
-             *parent=NULL,
-             *nd=new Nodo<H>(p,key);
-     while(tmp){
-       parent=tmp;
-       if(p == tmp->getPriority()) return this;
-       if(p>tmp->getPriority()) tmp=tmp->getRight();
-       else tmp=tmp->getLeft();
-     }
-     if(!parent){
-       root=nd;
-     }else{
-       if(p > parent->getPriority()) parent->setRight(nd);
-       else parent->setLeft(nd);
-     }
-     n++;
-     return this;
-   }
+  Nodo<H> *_minimo(Nodo<H> *x){
+    while(x->getLeft()) x=x->getLeft();
+    return x;
+  }
 
-   PBT<H> *del(H key){
-     _canc(root, key);
-     return this;
-   }
+  Nodo<H> *_successorePriority(Nodo<H> *x){
+    if(x->getRight()) return _minimo(x->getRight());
+    Nodo<H> *p=x->getParent();
+    while(p && p->getPriority()<x->getPriority()) p=p->getParent();
+    return p;
+  }
 
-   void print(){
-     _inoderReverse(root);
-     cout << endl;
-   }
+  Nodo<H> *_itSearch(Nodo<H> *x, H key){
+    Nodo<H> *tmp=_minimo(root);
+    while(tmp){
+      if(*tmp->getKey() == key) return tmp;
+      tmp= _successorePriority(tmp);
+    }
+    return NULL;
+  }
 
-   H find(int p){
-     Nodo<H> *tmp = _rfind(p, root);
-     if(!tmp) return 0;
-     return *tmp->getKey();
-   }
+  void _canc(Nodo<H> *nd, H key){
+    Nodo<H> *tmp=_itSearch(nd, key);
+    if(!tmp) return;
+    if(!tmp->getLeft() || !tmp->getRight()){
+      n--;
+      Nodo<H> *figlio=tmp->getRight(),
+              *p=tmp->getParent();
+      if(tmp->getLeft()) figlio=tmp->getLeft();
+      if(!p) root=figlio;
+      else{
+        if(tmp == p->getLeft()) p->setLeft(figlio);
+        else p->setRight(figlio);
+      }
+      if(figlio) figlio->setParent(p);
+    }else{
+      Nodo<H> *succ= _successorePriority(tmp);
+      tmp->setKey(*succ->getKey());
+      _canc(tmp->getRight(), *succ->getKey());
+    }
+  }
+
+public:
+  MyPBT(){
+    root=NULL;
+    n=0;
+  }
+
+  MyPBT<H> *ins(int priority, H key){
+    Nodo<H> *tmp=root,
+            *p=NULL,
+            *nd=new Nodo<H>(priority,key);
+    while(tmp){
+      p=tmp;
+      if(priority> tmp->getPriority()) tmp=tmp->getRight();
+      else tmp=tmp->getLeft();
+    }
+    if(!p) root=nd;
+    else{
+      if(priority>p->getPriority()) p->setRight(nd);
+      else p->setLeft(nd);
+    }
+    nd->setParent(p);
+    n++;
+    return this;
+  }
+
+  MyPBT<H> *del(H key){
+    _canc(root, key);
+    return this;
+  }
+
+  H find(int priority){
+    Nodo<H> *tmp = _rfind(root, priority);
+    if(!tmp) return 0;
+    return *tmp->getKey();
+  }
+
+  void print(){
+    _revInOrder(root);
+    cout<<endl;
+  }
+};
 
 
 
- };
 
 
- int main() {
-   PBT<int>  *p= new PBT<int>();
-   p->insert(10,5)->insert(7,3)->insert(8,13)->insert(13,1)->insert(6,2)->insert(9,7)->print();
-   p->del(13)->del(1)->print();
+int main(){
+  MyPBT<int> *p=new MyPBT<int>();
+  p->ins(10,5)->ins(7,3)->ins(8,13)->ins(13,1)->ins(6,2)->ins(9,7)->print();
+  p->del(13)->del(1)->print();
+  cout<<p->find(10);
 
- return 0;
- }
+  return 0;
+}
